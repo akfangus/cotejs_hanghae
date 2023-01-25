d29b94a119aad16acf36fd7deddba71a315acc0c
akfangus <39788340+akfangus@users.noreply.github.com>
2023-01-26 00:45:25 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -1,3 +1,6 @@
 function solution(n) {
-    return Math.ceil(n / 7)
+    //사람수 n이 주어졌을때 1조각이상 먹기위판 피자의 최소값?
+    //1~7 1판 , 2~14 2판, 15~21 3판... 개념이므로
+    return parseInt((n-1)/7) + 1
+    //return Math.ceil(n / 7)
 }
