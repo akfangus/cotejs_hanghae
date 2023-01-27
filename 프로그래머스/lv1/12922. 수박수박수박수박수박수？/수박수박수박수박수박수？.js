680a8982595c7348f6bd8876a0d233b77734b4fc
akfangus <39788340+akfangus@users.noreply.github.com>
2023-01-27 11:08:09 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,6 +1,5 @@
 function solution(n) {
-  return [...Array(n)]
-    .map((v, i) => i)
-    .map((v) => (v % 2 == 0 ? "수" : "박"))
+  return [...Array(n).keys()] //
+    .map((i) => (i % 2 == 0 ? "수" : "박"))
     .join("");
 }
