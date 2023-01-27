5ebe7e50f9c0ce630d8c1042e146e7494526f4d6
akfangus <39788340+akfangus@users.noreply.github.com>
2023-01-27 09:53:44 +0900

    [level 1] Title: 2016년, Time: 0.11 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -1,6 +1,4 @@
 function solution(a, b) {
-    let days =['SUN','MON','TUE','WED','THU','FRI','SAT']
-    let dt = new Date(`2016-${a}-${b}`)
-    return days[dt.getDay()]
-    
+    var answer = '';
+    return answer;
 }
