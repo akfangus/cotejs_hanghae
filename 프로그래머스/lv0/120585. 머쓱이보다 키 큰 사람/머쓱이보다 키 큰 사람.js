f902422ff445280cbfb7aff1f865fb7e6970e7d0
akfangus <39788340+akfangus@users.noreply.github.com>
2023-01-27 21:13:19 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,3 +1,12 @@
 function solution(array, height) {
-  return array.filter((a) => a > height).length;
+    let result = 0;
+
+  //array를 돌면서 height보다 큰값이 있으면 result를 ++ 해준다.
+  for (let i =0;i<array.length;i++){
+    if (array[i]> height){
+        result +=1
+    }
+  }
+  return result
+
 }
