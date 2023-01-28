3f038e1ded16e31bdaa9f7e289f83a214406cef2
akfangus <39788340+akfangus@users.noreply.github.com>
2023-01-28 16:38:58 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.45 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,12 +1,19 @@
 function solution(left, right) {
-    let sum=0
-    for(let target =left; target<=right;target++){
-        let temp = [...Array((Math.floor(target/2)))].map((v,i) => i+1).filter((a) => target % a ==0 ).length +1
-        let temp2 = (temp % 2 ==0) ? target : -target
-        sum +=temp2
-    }
-    return sum
- 
+
+  
+  let sum = 0; //13+14+15-16
+
+  for (let target = left; target <= right; target++) {
+    //13
+    let temp =
+      [...Array(Math.floor(target / 2))]  
+        .map((v, i) => i + 1)
+        .filter((a) => target % a == 0).length + 1;
 
 
+
+    let temp2 = temp % 2 == 0 ? target : -target;
+    sum += temp2;
+  }
+  return sum;
 }
