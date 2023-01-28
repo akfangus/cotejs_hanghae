cf002d9db5b7cb20aef9122979c06a89b9eaf3f2
akfangus <39788340+akfangus@users.noreply.github.com>
2023-01-28 15:33:55 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,27 +1,13 @@
 function solution(num) {
-  //num값을 받아주는 target변수 선언
-  let target = num;
+  //let target = num;
 
-  if (num == 1) return 0;
-  let cnt = 0; // 반복 횟수
-  //1-1, 1-2 결과값을 넣어주는 배열을 빈배열을 만든다
-  let arr = [];
+  //if (num == 1) return 0; // 이게 필요없어지겟고
+  //let cnt2 = 0;
+  let cnt = 0;
 
-  //계속 반복해서 500에 제한을 둬야하니 while문을 쓴다.\
-  while (cnt < 500) {
-    //1-1, 1-2 구현
-    if (target == 1) return arr.length;
-    if (target % 2 == 0) {
-      target = target / 2;
-      arr.push(target);
-    } else {
-      target = target * 3 + 1;
-      arr.push(target);
-    }
-    cnt += 1;
+  while (num != 1 && cnt != 500) {
+    num % 2 == 0 ? (num = num / 2) : (num = num * 3 + 1);
+    cnt++;
   }
-  //   console.log(arr);
-
-  // 배열의 길이를 return 하면 result가 된다.
-  return -1;
+  return num == 1 ? cnt : -1;
 }
