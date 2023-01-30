0208f1fd703a039b3463b8dd71850195c2006f8e
akfangus <39788340+akfangus@users.noreply.github.com>
2023-01-30 16:41:37 +0900

    [level 1] Title: 소수 만들기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
@@ -1,23 +1,19 @@
-function checkPrime(target) {
-    for (let i = 3; i <= Math.sqrt(target); i+=2) {
-        if (target % i === 0) return false;
-    }
-    return true;
-}
-
 function solution(nums) {
-    let answer = 0;
-
-    for(let i = 0; i < nums.length - 2; i++) {
-        for(let j = i + 1; j < nums.length - 1; j++) {
-            for(let k = j + 1; k < nums.length; k++) {
-                let target = nums[i] + nums[j] + nums[k];
-                if(target % 2 !== 0 && checkPrime(target)) {
-                    answer++;
-                }
-            }
-        }
+  let answer = 0;
+  let arr = [];
+  
+  for (let i = 0; i < nums.length-2; i++) {
+    for (let j = i + 1; j < nums.length- 1; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        result = nums[i] + nums[j] + nums[k];
+        //result가 소수냐? 아니면 answer +1
+        let cnt=0
+        for (let p = 1; p <=result; p++) {
+          if (result % p == 0) {cnt++;}
+          }
+          (cnt==2)? answer++:answer;
+        } 
+      }
     }
-    
-    return answer;
+  return answer;
 }
