ed8f2fb9e8c4c040e88970ff03078165c1b3f135
akfangus <39788340+akfangus@users.noreply.github.com>
2023-01-30 10:52:35 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -3,13 +3,16 @@ function solution(lottos, win_nums) {
   cnt = 0;
   zerocnt = 0;
 
-  for (let i = 0; i < lottos.length; i++) {
-    if (lottos[i] == 0) {
-      zerocnt += 1;
-      continue;
-    }
-    if (win_nums.includes(lottos[i])) cnt += 1;
-  }
+  //   for (let i = 0; i < lottos.length; i++) {
+  //     if (lottos[i] == 0) {
+  //       zerocnt += 1;
+  //       continue;
+  //     }
+  //     if (win_nums.includes(lottos[i])) cnt += 1;
+  //   }
+
+  cnt = lottos.filter((i, v) => win_nums.includes(i)).length;
+  zerocnt = lottos.filter((i, v) => i == 0).length;
 
   return [score[cnt + zerocnt], score[cnt]];
 }
