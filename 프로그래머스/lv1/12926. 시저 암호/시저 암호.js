2b7506d6a89fdc141545804f0667a1cfe8bfa36d
akfangus <39788340+akfangus@users.noreply.github.com>
2023-01-31 10:35:29 +0900

    [level 1] Title: 시저 암호, Time: 9.36 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -1,18 +1,14 @@
 function solution(s, n) {
-    var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var lower = "abcdefghijklmnopqrstuvwxyz";
-    var answer= '';
-
-    for(var i =0; i <s.length; i++){
-        var text = s[i];
-        if(text == ' ') {
-            answer += ' '; 
-            continue;
-        }
-        var textArr = upper.includes(text) ? upper : lower;
-        var index = textArr.indexOf(text)+n;
-        if(index >= textArr.length) index -= textArr.length;
-        answer += textArr[index];
+    let answer =""
+    for(let i=0;i<s.length;i++){
+        let charCode =" "
+        let char =s[i].charCodeAt()
+        if(char>=65&&char<=90&&char+n>90){
+                charCode = char+n-26
+        }else if(char>=97&&char<=122&&char+n>122){
+                charCode = char+n-26
+        }else {charCode = char+n}
+        answer+=s[i]!==" "?String.fromCharCode(charCode) : " "
     }
-    return answer;
+    return answer
 }
